5353b3fc51f4beaf11495d03509887615b222dd1
Lars Nysom <LANY@ucn.dk>
2022-11-10 08:33:44 +0100

    Added comments for first tast

--- BookModelTests/Story1Tests.cs
+++ BookModelTests/Story1Tests.cs
@@ -1,5 +1,10 @@
 
 
+using static System.Formats.Asn1.AsnWriter;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using System.ComponentModel;
+
 namespace BookModelTests
 {
     public class Story1Tests
@@ -9,6 +14,10 @@ namespace BookModelTests
         // 3. Implement just enough code (a stub/empty class) to make the code compile.
         // 4. Verify that your test fails
         // 5. Only implement just enough code to make the test pass.
-       
+
+        // Suggested tests
+        // * Make a store that contains at least three different books with the following attributes: ISBN(string),
+        //   title(text), author(text), price(positive number), number on stock(zero, or positive)
+        // * Add a book with invalid values(ISBN/Title/etc.) and make sure the collection throws an ArgumentException
     }
 }
